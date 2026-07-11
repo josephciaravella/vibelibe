04dde5e1a4f0004da93fe7c8dfbcc82aef9936a2
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2026-07-10 23:35:57 -0400

    call edge function to add song to playlist

--- lib/services/spotify_service.dart
+++ lib/services/spotify_service.dart
@@ -9,11 +9,36 @@ class SpotifyService {
     await _supabase.auth.signInWithOAuth(
       OAuthProvider.spotify,
       redirectTo: kIsWeb ? null : 'my.vibelibe.app://callback', 
-      scopes: 'playlist-read-private playlist-read-collaborative',
+      scopes: 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private',
       queryParams: {'show_dialog': 'true'},
       authScreenLaunchMode: kIsWeb 
           ? LaunchMode.platformDefault 
           : LaunchMode.externalApplication,
     );
+  }
+
+  Future<void> addTrackToPlaylist({
+    required String playlistId,
+    required String trackId,
+    required String trackTitle,
+    required String artistName,
+  }) async {
+    final response = await _supabase.functions.invoke(
+      'add-track-to-playlist',
+      body: {
+        'playlistId': playlistId,
+        'trackId': trackId,
+        'trackTitle': trackTitle,
+        'artistName': artistName,
+      },
+    );
+
+    if (response.status != 200) {
+      final data = response.data;
+      final String errorMsg = (data is Map) 
+          ? (data['error'] ?? data['details'] ?? 'Server returned status ${response.status}')
+          : 'Server returned status ${response.status}';
+      throw Exception(errorMsg);
+    }
   }
 }
